e01382e0145f477e4e95f31d5a18a1f813171368
Luis4978 <luis4978@hotmail.com>
2023-04-15 20:00:30 -0300

    Cuestiones a resolver reunio 150423

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,6 +9,14 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
+
+        /* Pendiente de la reunion 15/04/2023
+         * Pasar valores a base de datos.
+         * Determinar el apostador ganador.
+         * Resolver si acierta una ronda gana puntos extras.
+         * Resolver conexion a base de datos.
+         * resolver el metodo contaraciertos
+         */
         Path rutaEquipos = Paths.get("equiposqatar2022.csv");
         Path rutaPartidos = Paths.get("partidosqatar2022.csv");
         Path rutaApuestas = Paths.get("pronosticoqatar2022.csv");
@@ -23,7 +31,7 @@ public class App
         CargaArchivos.cargarArchivoApostadores(archivoApostadores, prode.getApostadores());
 
         prode.cargarTicketsApuestas();//carga los tickets de cada apostador
-        prode.mostrarAciertos(campeonato);//Muestra los aciertos de cada apostador por ronda
+        //prode.mostrarAciertos(campeonato);//Muestra los aciertos de cada apostador por ronda
         /*
          * Metodos para comprobar la carga correcta de los archivos
          */
