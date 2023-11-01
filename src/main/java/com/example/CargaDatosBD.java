3f682c31fa21c9c0e85f8d5e76e1963c32bdacb7
Luis4978 <luis4978@hotmail.com>
2023-11-01 10:08:39 -0300

    Cambios en CargaDatosBD

--- src/main/java/com/example/CargaDatosBD.java
+++ src/main/java/com/example/CargaDatosBD.java
@@ -7,8 +7,6 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.HashMap;
-
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
